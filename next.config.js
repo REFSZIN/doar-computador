575d3278c736f5e8d61504958deff739bca804d9
refszin <refwire@gmail.com>
2022-07-22 21:49:06 -0300

    Build 1ª Etapa - Bootstraping - Frontend
    Projeto usando Next.js
    Exibindo <H1> com "Doação de computadores usados"
    Pacote AXIOS
    Se alive for true, exibir em um <P> "API online", se false exibir "API offline"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
