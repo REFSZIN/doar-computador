bf50974aeb2a3d405f49c7a701673124aa033c33
refszin <refwire@gmail.com>
2022-07-22 22:40:20 -0300

    Fix meta tag in Head

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
         <meta name="description" content=" Doação de Computadores in Next.js ! :)Framer-motion, JSX and JavaScrip"/>
         <meta name="keywords" content="JSX, CSS, JavaScript and Framer-motion"/>
         <meta name="author" content="https://github.com/REFSZIN/"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />;
         <title>⚡ Doação de Computadores ⚡</title>
