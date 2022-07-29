b497f38443098d5448e83f6f73a31185b8026153
refszin <refwire@gmail.com>
2022-07-29 19:54:09 -0300

    Fix: Deploy Configs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,15 @@
 
 import React from "react";
 import styles from '../styles/Header.module.css';
-interface Propis{
-};
 
-export default function Header(props: any){
-  return (
-    <header className={styles.header}>
-        <nav>
-            <h1>DOAR COMPUTADORES</h1>
-            <span></span>
-        </nav>
-    </header>
+export default function Header(){
+    return (
+        <header className={styles.header}>
+            <nav>
+                <h1>DOAR COMPUTADORES</h1>
+                <span></span>
+            </nav>
+        </header>
     )
-};
+}
 
