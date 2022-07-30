87ee4feb094a9ae89a76773697eecf1cc5d32bee
refszin <refwire@gmail.com>
2022-07-29 22:00:08 -0300

    Styled Components Implement

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import styles from '../styles/Header.module.css';
-
+import { H1 ,Logo , Aa } from './Header.module';
 interface server {
     data: Boolean
 }
@@ -9,10 +9,11 @@ interface server {
 export default function Header(props: server){
     return (
         <header className={styles.header}>
-            <nav>
-                <h1>DOAR COMPUTADORES</h1>
-                <span>API {props.data ? "On" : "Off"}</span>
-            </nav>
+                <Aa >
+                    <Logo className={styles.H1} alt="Logo"/>
+                    <H1>CINEFLEX</H1>
+                    <span>API {props.data ? "On" : "Off"}</span>
+                </Aa>
         </header>
     )
 }
